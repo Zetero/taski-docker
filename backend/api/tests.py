f5acb88308edbaa4f3e28d601d863331cb5ac6e3
Zetero <80723607+Zetero@users.noreply.github.com>
2023-08-09 15:40:24 +0300

    Add tests and complete workflow conf

---
+++ backend/api/tests.py
@@ -0,0 +1,20 @@
+from http import HTTPStatus
+
+from api import models
+from django.test import Client, TestCase
+
+
+class TaskiAPITestCase(TestCase):
+
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        response = self.guest_client.get("/api/tasks/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        data = {"title": "Test", "desctiption": "Test"}
+        response = self.guest_client.post("/api/tasks/", data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertEqual(models.Task.objects.filter(Title="Test").exists())
